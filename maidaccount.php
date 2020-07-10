ddea36e9344b397ab555f103b78975f798bb6c91
kiam <kiam125@gmail.com>
2020-07-11 03:41:09 +0800

    Others: add table

--- maidaccount.php
+++ maidaccount.php
@@ -48,38 +48,88 @@ if($total != 0)
 
     </div>
 
-        <div class="well">
+    <div class="well">
       <div class="tab-content">
         <div class="tab-pane fade in active" id="tab1">
-        <table class="table">
-            <?php
-                if($total != 0)
-                {
-                    echo "
-                        <tr>
-                            <td>First Name</td>
-                            <td>".$fname."</td>
-                        </tr>
-                        <tr>
-                            <td>Last Name</td>
-                            <td>".$lname."</td>
-                        </tr>
-                        <tr>
-                            <td>Gender</td>
-                            <td>".$gender."</td>
-                        </tr>
-                        <tr>
-                            <td>Email</td>
-                            <td>".$email."</td>
-                        </tr>
-                    ";
-                }else{
-                    echo"No records found";
-                }
-            ?>
-          </table>
+            <table class="table">
+                <?php
+                    if($total != 0)
+                    {
+                        echo "
+                            <tr>
+                                <td>First Name</td>
+                                <td>".$fname."</td>
+                            </tr>
+                            <tr>
+                                <td>Last Name</td>
+                                <td>".$lname."</td>
+                            </tr>
+                            <tr>
+                                <td>Gender</td>
+                                <td>".$gender."</td>
+                            </tr>
+                            <tr>
+                                <td>Email</td>
+                                <td>".$email."</td>
+                            </tr>
+                        ";
+                    }else {
+                        echo"No records found";
+                    }
+                ?>
+            </table>
         </div>
+        <div class="tab-pane fade in active" id="tab2">
+            <table border="2" cellspacing="8">
+                <tr>
+                    <th>no</th>
+                    <th>Profile Picture</th>
+                    <th>Firstname</th>
+                    <th>Lastname</th>
+                    <th>Gender</th>
+                    <th>Option</th>
+                    <th>Hour(s)</th>
+                    <th></th>
+                </tr>
 
+                <?php
+                    include("connection.php");
+                    error_reporting(0);
+                    $query = "SELECT * FROM maid ";
+                    $data = mysqli_query($conn, $query);
+                    $total = mysqli_num_rows($data);
+
+                    if($total != 0)
+                    {
+                        while($result=mysqli_fetch_assoc($data))
+                        {
+                            echo "
+                                <tr>
+                                <td>".$result['ID']."</td>
+                            ";
+                            echo '<td><img src="'.$result['Pic'].'" width="100"/></td>';
+                            echo "
+                                <td>".$result['Firstname']."</td>
+                                <td>".$result['Lastname']."</td>
+                                <td>".$result['Gender']."</td>
+                            ";
+                            echo'<td>
+                                    <input type="datetime-local"/><br>
+                                </td>';
+                            echo'<td>
+                                    <input type="time"/><br>
+                                </td>';
+                            echo"
+                                <td><a href = 'adminedit.php?ID=$result[ID]' onclick='return checkedit()'> Cancel Bocking </td>
+                                </tr>
+                            ";
+                        }
+                    } else{
+                        echo"No records found";
+                    }
+                ?>
+            </table>
+        </div>
       </div>
     </div>
 
